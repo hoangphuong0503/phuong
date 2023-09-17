35ea79e71c5d0f6c1a795eb4fe9044aa0051e0f6
Le Tri Luan <letriluan12345@gmail.com>
2023-09-17 23:33:23 +0930

    phuong

---
+++ question16.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/ian-knight-uofa/git-practice-02.git 
+cd git-practice-02.git 
+git checkout branch2
+git merge origin/branch3
+git add .
+git commit -m "question16"
+git branch -d branch3
